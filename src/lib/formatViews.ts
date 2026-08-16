e66afacb38865cff9a42093d72c3478c2fbab236
Emily Edwards <Emilydedwards@gmail.com>
2026-08-15 23:10:37 -0500

    Delete src directory

--- src/lib/formatViews.ts
+++
@@ -1,4 +0,0 @@
-export function formatViews(views: number, includeLabel = true) {
-  const formatted = new Intl.NumberFormat('en-US').format(views);
-  return includeLabel ? `${formatted} views` : formatted;
-}
